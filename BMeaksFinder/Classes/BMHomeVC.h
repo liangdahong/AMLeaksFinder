5beed658454344f797d4b257e9304f4f803f4664
梁大红 <ios@liangdahong.com>
2020-05-13 10:34:22 +0800

    init

---
+++ BMeaksFinder/Classes/BMHomeVC.h
@@ -0,0 +1,17 @@
+//
+//  BMHomeVC.h
+//  BMeaksFinder
+//
+//  Created by liangdahong on 2020/5/13.
+//  Copyright © 2020 liangdahong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BMHomeVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BMeaksFinderTests/BMeaksFinderTests.m
@@ -0,0 +1,37 @@
+//
+//  BMeaksFinderTests.m
+//  BMeaksFinderTests
+//
+//  Created by liangdahong on 2020/5/13.
+//  Copyright © 2020 liangdahong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BMeaksFinderTests : XCTestCase
+
+@end
+
+@implementation BMeaksFinderTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
