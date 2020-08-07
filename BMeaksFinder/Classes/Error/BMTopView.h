4971be25acd4bbe415e0bb0970dda50277422538
梁大红 <hi@liangdahong.com>
2020-08-07 14:05:22 +0800

    Initial commit

---
+++ BMeaksFinder/Classes/Error/BMTopView.h
@@ -0,0 +1,15 @@
+//
+//  BMTopView.h
+//  BMeaksFinder
+//
+//  Created by liangdahong on 2020/5/13.
+//  Copyright © 2020 liangdahong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMTopView : UIView
+
+@property (nonatomic, copy) dispatch_block_t didClickBlock; ///< didClickBlock
+
+@end
---
+++ README.md
@@ -0,0 +1,2 @@
+# BMeaksFinder
+BMeaksFinder
