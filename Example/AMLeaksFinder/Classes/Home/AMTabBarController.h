3186d48d5607581ff0a37a627b9de2e1e7d8b48b
liangdahong <hi@liangdahong.com>
2021-01-06 23:12:43 +0800

    优化切换 root

---
+++ Example/AMLeaksFinder/Classes/Home/AMTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  AMTabBarController.h
+//  AMLeaksFinder_Example
+//
+//  Created by liangdahong on 2021/1/5.
+//  Copyright © 2021 ios@liangdahong.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AMTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
